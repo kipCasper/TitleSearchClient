ef9381653ca2ace8942e4da00554dfe9cddfbb29
David <dmbaker2011@gmail.com>
2020-04-30 13:12:19 -0400

    refine search

--- src/app/Components/details-page/details-page.component.ts
+++ src/app/Components/details-page/details-page.component.ts
@@ -11,6 +11,14 @@ export class DetailsPageComponent implements OnInit {
   @Input() title: Title = new Title();
   @Output() backClicked = new EventEmitter();
 
+  public storyLimit = 1;
+  public showStoryText = 'show all';
+
+  public awardLimit = 5;
+  public showAwardText = 'show all';
+  public castLimit = 10;
+  public showCastText = 'show all';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -18,5 +26,35 @@ export class DetailsPageComponent implements OnInit {
 
   public goBack() {
     this.backClicked.emit();
+  }
+
+  public showStories() {
+    if (this.showStoryText === 'show all') {
+      this.storyLimit = this.title.storylines.length;
+      this.showStoryText = 'show less'
+    } else {
+      this.storyLimit = 1;
+      this.showStoryText = 'show all';
+    }
+  }
+
+  showCast() {
+    if (this.showCastText === 'show all') {
+      this.castLimit = this.title.participants.length;
+      this.showCastText = 'show less';
+    } else {
+      this.castLimit = 10;
+      this.showCastText = 'show all';
+    }
+  }
+
+  showAwards() {
+    if (this.showAwardText === 'show all') {
+      this.awardLimit = this.title.awards.length;
+      this.showAwardText = 'show less';
+    } else {
+      this.awardLimit = 5;
+      this.showAwardText = 'show all';
+    }
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TitleService } from './Services/title.service';
 import { Title } from './Models/Title';
+import { Subject, generate } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,7 @@ export class AppComponent implements OnInit, OnDestroy {
   
   public fTitles: Array<Title>;
   private titles: Array<Title>;
+  private searchChanged: Subject<string> = new Subject<string>();
   
   constructor(private titleService: TitleService) { }
 
@@ -23,8 +26,12 @@ export class AppComponent implements OnInit, OnDestroy {
     this.titleSubscription = this.titleService.getTitles().subscribe(titles => {
       this.titles = titles;
       this.fTitles = titles;
+    });
 
-      console.log(titles);
+    this.searchChanged.pipe(
+      debounceTime(500)
+    ).subscribe(() => {
+      this.search();
     });
   }
 
@@ -33,7 +40,21 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   public search() {
-    this.fTitles = this.titles.filter(title => title.titleName.toUpperCase().indexOf(this.searchInput.toUpperCase()) > -1);
+    if (this.selected === 'title') {
+      this.fTitles = this.titles.filter(title => title.titleName.toUpperCase().includes(this.searchInput.toUpperCase()));
+    } else if (this.selected === 'genre') {
+      this.fTitles = this.titles.filter(title => {
+        return title.genres.some(genre => genre.toUpperCase().includes(this.searchInput.toUpperCase())) === true
+        });
+    } else if (this.selected === 'cast') {
+      this.fTitles = this.titles.filter(title => {
+        return title.participants.some(part => part.name.toUpperCase().includes(this.searchInput.toUpperCase())) === true
+      });
+    }
+  }
+
+  public inputChanged() {
+    this.searchChanged.next(this.searchInput);
   }
 
   public showDetails(title: Title) {
@@ -43,5 +64,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   public showIndex() {
     this.displayDetails = false;
+    this.searchInput = '';
+    this.fTitles = this.titles;
   }
 }
