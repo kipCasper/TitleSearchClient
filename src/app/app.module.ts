bf4f2ccf968249c994d591a97fea978691b9997b
David <dmbaker2011@gmail.com>
2020-05-04 15:51:38 -0400

    clean up app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './Components/title/title.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DetailsPageComponent } from './components/details-page/details-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+
+import { TitleComponent } from './Components/title/title.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +27,12 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatSelectModule,
     MatInputModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule,
-    FormsModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
